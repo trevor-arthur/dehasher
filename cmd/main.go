768a917de7e47a664c09d7fa26180ef471d946a0
trevor-arthur <trevor.spaniola@protonmail.com>
2023-05-19 14:09:30 -0700

    print or csv argument options

--- cmd/main.go
+++ cmd/main.go
@@ -21,16 +21,17 @@ func main() {
 	apiKey := os.Getenv("DEHASHED_API_KEY")
 	apiEmail := os.Getenv("DEHASHED_API_EMAIL")
 
-	// Get search query from arg (i.e. ./dehasher "email:@trevorarthur.com")
-	query := os.Args[1]
+	// Get search query from arg (i.e. ./dehasher -c "email:@trevorarthur.com")
+	outputType := os.Args[1]
+	query := os.Args[2]
 
 	// Get email search results
 	results := pkg.Search(query, apiEmail, apiKey)
 
-	// Print email search results
-	//pkg.PrintResults(results)
-
-	// Output results into csv file
-	pkg.CSVresults(results)
+	if outputType == "-c" {
+		pkg.CSVresults(results)
+	} else if outputType == "-p" {
+		pkg.PrintResults(results)
+	}
 
 }
